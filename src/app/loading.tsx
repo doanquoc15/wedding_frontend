d7a6c133e7bdf1a63ececec8362fa9cb1785a656
ltwphuquoc <quocdoan19t2@gmail.com>
2023-10-01 23:59:13 +0700

    test: rename repo

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -5,7 +5,7 @@ import LoadingIcon from "@/statics/svg/ic-loading.svg";
 
 const Loading = () => {
   return (
-    <div className="w-ful bg-[rgba(0,0,0,0.15)]">
+    <div className="w-full">
       <div className="flex justify-center items-center">
         <Image alt="LoadingIcon" src={LoadingIcon} />
       </div>
