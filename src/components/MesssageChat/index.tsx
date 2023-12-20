83601ba24e9ef726a1cdff88f2d87f591b9020ff
doanquoc15 <quocdoan19t2@gmail.com>
2023-12-20 23:32:33 +0700

    fix: fix chat message

--- src/components/MesssageChat/index.tsx
+++ src/components/MesssageChat/index.tsx
@@ -1,10 +1,10 @@
 "use client";
-import { FacebookProvider, CustomChat } from "react-facebook";
+import { CustomChat, FacebookProvider } from "react-facebook";
 
 export default function CustomChatMessage() {
   return (
-    <FacebookProvider appId="356231870419739">
-      <CustomChat pageId="135217426340957" />
+    <FacebookProvider appId="1528171267932881">
+      <CustomChat pageId="135217426340957"/>
     </FacebookProvider>
   );
 }
