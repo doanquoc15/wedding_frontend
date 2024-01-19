32b90e58a991574b68328066c9a5b70883b0874d
doanquoc15 <quocdoan19t2@gmail.com>
2024-01-19 09:03:31 +0700

    fix: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -44,12 +44,5 @@ export const config = {
   matcher: [
     "/((?!api|_next/static|_next/image|favicon.ico|auth/login).*)",
     "/partner/:path*",
-    {
-      source: "/((?!api|_next/static|_next/image|favicon.ico).*)",
-      missing: [
-        { type: "header", key: "next-router-prefetch" },
-        { type: "header", key: "purpose", value: "prefetch" },
-      ],
-    },
   ],
 };
