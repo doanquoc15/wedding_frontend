a8c150f2d2039c4ccd3caf9d49e78b53bd52ccbe
doanquoc15 <quocdoan19t2@gmail.com>
2024-01-19 08:53:15 +0700

    fix: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,15 +7,13 @@ export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
   const role = request.cookies.get("role")?.value;
 
+  // Check if the user is logged in
   if (role) {
     if (pathname === "/dang-nhap" || pathname === "/dang-ky") {
       return NextResponse.redirect(new URL("/", request.url));
     }
 
-    if (role === "CUSTOMER") {
-      return NextResponse.redirect(new URL("/", request.url));
-    }
-
+    // Check admin routes for admin role
     if (pathname.startsWith("/admin") && role !== "ADMIN") {
       return NextResponse.redirect(new URL("/", request.url));
     } else if (
@@ -25,16 +23,29 @@ export function middleware(request: NextRequest) {
       return NextResponse.redirect(new URL("/admin/dashboard", request.url));
     }
 
+    // Allow access to non-admin routes and admin routes for admin users
     return NextResponse.next();
   } else {
+    // Redirect to signin for protected routes when the user is not logged in
     if (protectedRoutes.includes(pathname) || pathname.startsWith("/admin")) {
       return NextResponse.redirect(new URL("/dang-nhap", request.url));
     }
+
+    // Allow access to public routes when the user is not logged in
     return NextResponse.next();
   }
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|auth/login).*)",
-    "/partner/:path*",],
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|auth/login).*)",
+    "/partner/:path*",
+    {
+      source: "/((?!api|_next/static|_next/image|favicon.ico).*)",
+      missing: [
+        { type: "header", key: "next-router-prefetch" },
+        { type: "header", key: "purpose", value: "prefetch" },
+      ],
+    },
+  ],
 };
